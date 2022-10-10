f1f2c542a00fadcb477eb00080f97c31ecd52c2a
Gilson Gindre Jr <gindrejunior@hotmail.com>
2022-10-10 20:58:55 -0300

    feat: created address model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    zip_code = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    neighborhood = models.CharField(max_length=30)
+    street = models.CharField(max_length=30)
+    house_number = models.CharField(max_length=10)
+    complement = models.CharField(max_length=100)
