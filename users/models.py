1e111fe54bb1d9def47854dfdeb5fff54d39bf34
Gilson Gindre Jr <gindrejunior@hotmail.com>
2022-10-10 23:20:42 -0300

    feat: created custom user manager

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import gettext_lazy as _
 from stdimage import StdImageField
+from django.contrib.auth.hashers import make_password
+from django.apps import apps
 
 
 def get_file_path(_instance, filename):
@@ -21,16 +23,58 @@ class Address(models.Model):
     complement = models.CharField(_('complement'), max_length=100)
     
 
+class CustomUserManager(UserManager):
+    
+    use_in_migrations = True
+    
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+    
+
 class User(AbstractUser):
     username = None
     first_name = models.CharField(_('first name'), max_length=150)
     last_name = models.CharField(_('last name'), max_length=150)
     email = models.EmailField(max_length=50, unique=True)
     cpf = models.CharField(max_length=11)
-    address = models.ForeignKey(Address, verbose_name=_("address"), on_delete=models.CASCADE)
-    image = StdImageField(_('image'), upload_to=get_file_path, variations={'thumb': (400, 400)})
+    address = models.ForeignKey(Address, verbose_name=_("address"), on_delete=models.CASCADE, null=True, blank=True)
+    image = StdImageField(_('image'), upload_to=get_file_path, variations={'thumb': (400, 400)}, null=True, blank=True)
 
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'cpf']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'cpf']
     
     def __str__(self):
-        return f'User: first_name: {self.first_name} last_name: {self.last_name} email: {self.email}'
+        return self.email
+    
+    objects = CustomUserManager()
