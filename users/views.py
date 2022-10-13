50f467a733f273986aec6bb94fcf8cd27702cf29
Gilson Gindre Jr <gindrejunior@hotmail.com>
2022-10-13 15:39:54 -0300

    feat: created users and addresses view sets

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Address, User
+
+from .serializers import AddressSerializer, UsersSerializer
+
+
+class AddressViewSet(ModelViewSet):
+    serializer_class = AddressSerializer
+    queryset = Address.objects.all()
+
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UsersSerializer
+    queryset = User.objects.all()
