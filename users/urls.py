62a29ea996680c6b0549dfd69d479c019abd4796
GilsonJr <gindrejunior@hotmail.com>
2022-11-08 22:40:27 -0300

    feat: register, login, user info and logout routes

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,19 @@
+from django.urls import path, include
 from rest_framework import routers
 
-from .views import AddressViewSet, UserViewSet
+from knox import views as knox_views
+from .views import AddressViewSet, UserViewSet, RegisterAPI, LoginAPI, UserAPI
 
 users_router = routers.SimpleRouter()
 
 users_router.register('addresses', AddressViewSet)
 users_router.register('users', UserViewSet)
-urlpatterns = users_router.urls
+
+urlpatterns = [
+    path('auth/register', RegisterAPI.as_view()),
+    path('auth/login', LoginAPI.as_view()),
+    path('auth/user', UserAPI.as_view()),
+    path('auth/logout', knox_views.LogoutView.as_view())
+]
+
+urlpatterns += users_router.urls
