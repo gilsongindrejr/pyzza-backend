e9a6767713acd6ed391da82124251bcee4b9938b
GilsonJr <gindrejunior@hotmail.com>
2022-11-08 22:38:06 -0300

    feat: register and login serializer and changes in user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, Serializer, CharField, ValidationError
+from django.contrib.auth import authenticate
 
 from .models import Address, User
 
@@ -9,8 +10,37 @@ class AddressSerializer(ModelSerializer):
         fields = ['zip_code', 'state', 'city', 'neighborhood', 'street', 'house_number', 'complement']
 
 
-class UsersSerializer(ModelSerializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'is_staff']
-        
+        fields = ['id', 'email', 'first_name', 'last_name']
+
+
+class RegisterSerializer(ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password', 'cpf']
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            cpf=validated_data['cpf'],
+        )
+        return user
+
+
+class LoginSerializer(Serializer):
+    email = CharField()
+    password = CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise ValidationError('Incorrect Credentials')
