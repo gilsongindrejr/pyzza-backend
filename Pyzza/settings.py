3db13fa5d27203b273c1378d1ba17fd9659fd239
GilsonJr <gindrejunior@hotmail.com>
2022-11-08 22:35:47 -0300

    feat: added knox to installed and set authentication class

--- Pyzza/settings.py
+++ Pyzza/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
-    'rest_framework.authtoken',
+    'knox',
     
     'corsheaders',
     
@@ -142,11 +142,8 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES' : [
-        'rest_framework.authentication.TokenAuthentication'
+        'knox.auth.TokenAuthentication'
     ],
-    'DEFAULT_PERMISSION_CLASSES' : [
-        'rest_framework.permissions.IsAdminUser'
-    ]
 }
 
 """
