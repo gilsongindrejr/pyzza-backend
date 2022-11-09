002a9af79b75a2a2e28dc10cb771d0ae4e690272
GilsonJr <gindrejunior@hotmail.com>
2022-11-08 22:38:51 -0300

    feat: register, login and user view

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+from knox.auth import AuthToken
 
 from .models import Address, User
 
-from .serializers import AddressSerializer, UsersSerializer
+from .serializers import AddressSerializer, UserSerializer, RegisterSerializer, LoginSerializer
 
 
 class AddressViewSet(ModelViewSet):
@@ -11,5 +15,39 @@ class AddressViewSet(ModelViewSet):
 
 
 class UserViewSet(ModelViewSet):
-    serializer_class = UsersSerializer
+    serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class RegisterAPI(GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            'user': UserSerializer(user, context=self.get_serializer_context()).data,
+        })
+
+
+class LoginAPI(GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        _, token = AuthToken.objects.create(user)
+        return Response({
+            'user': UserSerializer(user, context=self.get_serializer_context()).data,
+            'token': token
+        })
+
+
+class UserAPI(RetrieveAPIView):
+    permission_classes = [IsAuthenticated,]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
