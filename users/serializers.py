774d0842f3e69bc11558ae81ae7a8071033e35b4
GilsonJr <gindrejunior@hotmail.com>
2022-11-12 14:56:06 -0300

    feat: user serializer gives cpf and image info

--- users/serializers.py
+++ users/serializers.py
@@ -13,7 +13,7 @@ class AddressSerializer(ModelSerializer):
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name']
+        fields = ['id', 'email', 'first_name', 'last_name', 'cpf', 'image']
 
 
 class RegisterSerializer(ModelSerializer):
