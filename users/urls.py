6e9d75a1ba4268a3565f18ec06f3944e0d1eefa3
GilsonJr <gindrejunior@hotmail.com>
2022-11-13 20:31:15 -0300

    feat: added change password, update user and logout urls

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from knox import views as knox_views
-from .views import AddressViewSet, UserViewSet, RegisterAPI, LoginAPI, UserAPI
+from .views import AddressViewSet, UserViewSet, RegisterAPI, LoginAPI, UserAPI, ChangePasswordAPI, UpdateUserAndAddressAPI
 
 users_router = routers.SimpleRouter()
 
@@ -13,7 +13,9 @@ urlpatterns = [
     path('auth/register', RegisterAPI.as_view()),
     path('auth/login', LoginAPI.as_view()),
     path('auth/user', UserAPI.as_view()),
-    path('auth/logout', knox_views.LogoutView.as_view())
+    path('auth/change_password', ChangePasswordAPI.as_view()),
+    path('auth/update_user', UpdateUserAndAddressAPI.as_view()),
+    path('auth/logout', knox_views.LogoutView.as_view()),
 ]
 
 urlpatterns += users_router.urls
